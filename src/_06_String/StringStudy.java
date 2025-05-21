0051ca22d73a7466b506580c7a9849730e842b72
earlyerin <rin050301@gmail.com>
2025-05-21 10:05:28 +0900

    feat: StringStudy

---
+++ src/_06_String/StringStudy.java
@@ -0,0 +1,18 @@
+package _06_String;
+
+public class StringStudy {
+    public static void main(String[] args) {
+
+        String str1 = "Java";
+        String str2 = new String("Java");
+
+        String literalStr1 = "Hello World";
+        String literalStr2 = "Hello World";
+
+        System.out.println(literalStr1 == literalStr2); // true
+
+
+
+
+    }
+}
