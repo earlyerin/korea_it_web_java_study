32e6a823a2428462f276a8f130da1100dbe2447b
earlyerin <rin050301@gmail.com>
2025-05-21 21:46:32 +0900

    feat: Overloading Quiz

--- src/_18_Overloading/Overloading.java
+++ src/_18_Overloading/Overloading.java
@@ -6,7 +6,7 @@ public class Overloading {
     /*
     ***** 오버로딩 ***** (면접 질문)
     메소드 오버로딩 : 같은 이름의 메소드를 여러 개 선언
-    단, 매개변수의 타입이 다르거나 개수가 달라야한다.
+    단, 매개변수의 타입이 다르거나 개수가 달라야하고 리턴타입은 동일해야한다.
      */
     public static int getPower(int number){
         return number * number;
@@ -42,6 +42,10 @@ public class Overloading {
         printInfo("이예린", 21);
         printInfo("이예린", 21, "admin");
 
+        //add() 메소드 오버로딩
+        add(10,20);
+        add(123.45, 467.75);
+        add("1", "2");
 
     }
 
@@ -65,5 +69,23 @@ public class Overloading {
         System.out.println("나이 : " + age);
         System.out.println("이메일 : " + email);
     }
+
+    /* 퀴즈
+    메소드명 : add()
+    1. int와 int 덧셈
+    2. double과 double 덧셈
+    3. 문자열숫자와 문자열숫자 덧셈
+     */
+    public static void add(int a, int b) {
+        System.out.println(a + b);
+    }
+
+    public static void add(double a, double b) {
+        System.out.println(a + b);
+    }
+    public static void add(String a, String  b) {
+        System.out.println(Integer.parseInt(a) + Integer.parseInt(b));
+    }
+
 
 }
