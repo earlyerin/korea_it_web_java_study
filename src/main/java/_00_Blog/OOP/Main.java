deb70d90d2a7285c57c35d20fa5fbda5449b2e43
earlyerin <rin050301@gmail.com>
2025-06-10 02:58:37 +0900

    feat: Object

--- src/main/java/_00_Blog/OOP/Main.java
+++ src/main/java/_00_Blog/OOP/Main.java
@@ -2,16 +2,15 @@ package _00_Blog.OOP;
 
 public class Main {
     public static void main(String[] args) {
-
         NewRemoteControl newRemoteControl = new NewRemoteControl();
+
         newRemoteControl.power();
         newRemoteControl.channelOnDown(97);
-        newRemoteControl.channelOnUp(5);
+        newRemoteControl.channelOnUp(2);
         newRemoteControl.fixedChannel();
         newRemoteControl.channelOnUp(7);
         newRemoteControl.changeableChannel();
         newRemoteControl.channelOnUp(7);
         newRemoteControl.power();
-
     }
 }
